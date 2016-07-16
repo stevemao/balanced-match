48ac88a538966b3e62f949122569dace0a70e28c
Steve Mao <maochenyan@gmail.com>
2016-07-16 22:02:02 +1000

    add some annotation

--- index.js
+++ index.js
@@ -21,34 +21,52 @@ function maybeMatch(reg, str) {
 
 balanced.range = range;
 function range(a, b, str) {
+  debugger
   var begs, beg, left, right, result;
+  // initial a index and b index
   var ai = str.indexOf(a);
   var bi = str.indexOf(b, ai + 1);
+  // initial index. index will be pointing at a or b
   var i = ai;
 
+  // make sure there is an a in the str and b is not at the first position
   if (ai >= 0 && bi > 0) {
+    // collection of a indexes
     begs = [];
+    // left position
     left = str.length;
 
+    // i must be >=0 but < than char length
+    // and there must be no result
     while (i < str.length && i >= 0 && ! result) {
       if (i == ai) {
+        // add another a index to begs
         begs.push(i);
+        // if there's another a in str
         ai = str.indexOf(a, i + 1);
+        // if there is only one a, we got an result!
       } else if (begs.length == 1) {
         result = [ begs.pop(), bi ];
       } else {
+        // there are more than one potential a indexes
         beg = begs.pop();
+        // if beg is smaller than left
         if (beg < left) {
+          // left becomes beg
           left = beg;
+          // right becomes b index
           right = bi;
         }
 
+        // if there's another b in str
         bi = str.indexOf(b, i + 1);
       }
 
+      // if a index is greater than 0 and smaller than b index, i become a index.
       i = ai < bi && ai >= 0 ? ai : bi;
     }
 
+    // if there are still something in begs
     if (begs.length) {
       result = [ left, right ];
     }
@@ -56,3 +74,5 @@ function range(a, b, str) {
 
   return result;
 }
+
+range('{', '}', 'e{{few}bb')
