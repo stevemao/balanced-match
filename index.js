b1378606c03c34ae4b183afd8467d6bbb8c7f3f5
Steve Mao <maochenyan@gmail.com>
2016-07-17 10:34:17 +1000

    temp

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 module.exports = balanced;
 function balanced(a, b, str) {
+
 }
 
 balanced.range = range;
 function range(a, b, str) {
+  var ai = str.indexOf(a);
+  var bi = str.indexOf(b);
+
+  if (ai === -1) {
+    return;
+  }
+
+  if (bi < ai) {
+    bi = str.indexOf(b, ai + 1);
+  }
+
+  if (bi === -1) {
+    return;
+  }
+
+  var newAi = str.indexOf(a, ai + 1) {
+    if (newAi !== -1) {
+      ai = newAi;
+    }
+  }
 }
