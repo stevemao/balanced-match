7780852851e69df50a48f37d2ab997f95f8f33e2
Steve Mao <maochenyan@gmail.com>
2016-07-17 14:34:54 +1000

    add old looping

--- test/bench.js
+++ test/bench.js
@@ -1,37 +1,37 @@
 'use strict';
-var indexOf = require('../');
-var looping = require('./looping');
+var stevesLooping = require('../');
+var oldLooping = require('./old-looping');
 
-bench('indexOf', function() {
-  indexOf('{', '}', 'pre{in{nest}}post');
-  indexOf('{', '}', '{{{{{{{{{in}post');
-  indexOf('{', '}', 'pre{body{in}post');
-  indexOf('{', '}', 'pre}{in{nest}}post');
-  indexOf('{', '}', 'pre{body}between{body2}post');
-  indexOf('{', '}', 'nope');
-  indexOf('<b>', '</b>', 'pre<b>in<b>nest</b></b>post');
-  indexOf('<b>', '</b>', 'pre</b><b>in<b>nest</b></b>post');
-  indexOf('{{', '}}', 'pre{{{in}}}post');
-  indexOf('{{{', '}}', 'pre{{{in}}}post');
-  indexOf('{', '}', 'pre{{first}in{second}post');
-  indexOf('<?', '?>', 'pre<?>post');
-  indexOf(/\{/, /\}/, 'nope');
-  indexOf(/\s+\{\s+/, /\s+\}\s+/, 'pre  {   in{nest}   }  post');
+bench('stevesLooping', function() {
+  stevesLooping('{', '}', 'pre{in{nest}}post');
+  stevesLooping('{', '}', '{{{{{{{{{in}post');
+  stevesLooping('{', '}', 'pre{body{in}post');
+  stevesLooping('{', '}', 'pre}{in{nest}}post');
+  stevesLooping('{', '}', 'pre{body}between{body2}post');
+  stevesLooping('{', '}', 'nope');
+  stevesLooping('<b>', '</b>', 'pre<b>in<b>nest</b></b>post');
+  stevesLooping('<b>', '</b>', 'pre</b><b>in<b>nest</b></b>post');
+  stevesLooping('{{', '}}', 'pre{{{in}}}post');
+  stevesLooping('{{{', '}}', 'pre{{{in}}}post');
+  stevesLooping('{', '}', 'pre{{first}in{second}post');
+  stevesLooping('<?', '?>', 'pre<?>post');
+  // stevesLooping(/\{/, /\}/, 'nope');
+  // stevesLooping(/\s+\{\s+/, /\s+\}\s+/, 'pre  {   in{nest}   }  post');
 });
 
-bench('looping', function() {
-  looping('{', '}', 'pre{in{nest}}post');
-  looping('{', '}', '{{{{{{{{{in}post');
-  looping('{', '}', 'pre{body{in}post');
-  looping('{', '}', 'pre}{in{nest}}post');
-  looping('{', '}', 'pre{body}between{body2}post');
-  looping('{', '}', 'nope');
-  looping('<b>', '</b>', 'pre<b>in<b>nest</b></b>post');
-  looping('<b>', '</b>', 'pre</b><b>in<b>nest</b></b>post');
-  looping('{{', '}}', 'pre{{{in}}}post');
-  looping('{{{', '}}', 'pre{{{in}}}post');
-  looping('{', '}', 'pre{{first}in{second}post');
-  looping('<?', '?>', 'pre<?>post');
-  looping(/\{/, /\}/, 'nope');
-  looping(/\s+\{\s+/, /\s+\}\s+/, 'pre  {   in{nest}   }  post');
+bench('oldLooping', function() {
+  oldLooping('{', '}', 'pre{in{nest}}post');
+  oldLooping('{', '}', '{{{{{{{{{in}post');
+  oldLooping('{', '}', 'pre{body{in}post');
+  oldLooping('{', '}', 'pre}{in{nest}}post');
+  oldLooping('{', '}', 'pre{body}between{body2}post');
+  oldLooping('{', '}', 'nope');
+  oldLooping('<b>', '</b>', 'pre<b>in<b>nest</b></b>post');
+  oldLooping('<b>', '</b>', 'pre</b><b>in<b>nest</b></b>post');
+  oldLooping('{{', '}}', 'pre{{{in}}}post');
+  oldLooping('{{{', '}}', 'pre{{{in}}}post');
+  oldLooping('{', '}', 'pre{{first}in{second}post');
+  oldLooping('<?', '?>', 'pre<?>post');
+  // oldLooping(/\{/, /\}/, 'nope');
+  // oldLooping(/\s+\{\s+/, /\s+\}\s+/, 'pre  {   in{nest}   }  post');
 });
